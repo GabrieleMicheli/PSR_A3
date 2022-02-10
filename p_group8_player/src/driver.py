bcad743fe1f29fc7a9af5ce125253a772055d12c
manuelgitgomes <gomesnelito@gmail.com>
2022-02-10 18:51:27 +0000

    #2 Added support for driver.py

--- p_group8_player/src/driver.py
+++ p_group8_player/src/driver.py
@@ -6,15 +6,20 @@ import rospy
 import tf2_ros
 from geometry_msgs.msg import Twist, PoseStamped
 
+
 class Driver():
     def __init__(self):
         self.team = 'Not defined'
+        self.prey = 'Not defined'
+        self.hunter = 'Not defined'
         # Getting the parameters from the YAML file
         red_players_list = rospy.get_param('/red_players')
         green_players_list = rospy.get_param('/green_players')
         blue_players_list = rospy.get_param('/blue_players')
         self.name = rospy.get_name()
-        self.name = self.name.strip('/') # removing slash
+        self.name = self.name.strip('/')  # removing slash
+        self.name = self.name.strip('/driver')  # removing slash
+        rospy.loginfo('My player name is ' + self.name)
         self.getTeam(red_players_list, green_players_list, blue_players_list)
         self.information(red_players_list, green_players_list, blue_players_list)
 
@@ -25,7 +30,7 @@ class Driver():
         self.tf_buffer = tf2_ros.Buffer()
         self.listener = tf2_ros.TransformListener(self.tf_buffer)
 
-        self.publisher_command = rospy.Publisher('p_randomName/cmd_vel', Twist, queue_size=1)
+        self.publisher_command = rospy.Publisher('/' + self.name + '/cmd_vel', Twist, queue_size=1)
         self.timer = rospy.Timer(rospy.Duration(0.1), self.sendCommandCallback)
         self.goal_subscriber = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.goalReceivedCallback)
 
@@ -34,32 +39,38 @@ class Driver():
 
         if self.name in red_players_list:
             self.team = 'Red'
+            self.prey = 'Blue'
+            self.hunter = 'Green'
         elif self.name in green_players_list:
             self.team = 'Green'
+            self.prey = 'Red'
+            self.hunter = 'Blue'
         elif self.name in blue_players_list:
             self.team = 'Blue'
+            self.prey = 'Green'
+            self.hunter = 'Red'
         else:
             self.team = 'Joker'
 
     def information(self, red_players_list, green_players_list, blue_players_list):
         if self.team == 'Red':
-            print("My name is "+ str(self.name) +". I am team " + str(self.team) + ". I am hunting " + str(green_players_list) + " and fleeing from " + str(blue_players_list))
+            rospy.loginfo("My name is " + str(self.name) + ". I am team " + str(self.team) + ". I am hunting " + str(
+                green_players_list) + " and fleeing from " + str(blue_players_list))
         elif self.team == 'Green':
-            print("My name is " + str(self.name) + ". I am team " + str(self.team) + ". I am hunting " + str(
+            rospy.loginfo("My name is " + str(self.name) + ". I am team " + str(self.team) + ". I am hunting " + str(
                 blue_players_list) + " and fleeing from " + str(red_players_list))
         elif self.team == 'Blue':
-            print("My name is " + str(self.name) + ". I am team " + str(self.team) + ". I am hunting " + str(
+            rospy.loginfo("My name is " + str(self.name) + ". I am team " + str(self.team) + ". I am hunting " + str(
                 red_players_list) + " and fleeing from " + str(green_players_list))
         else:
-            print('You are a joker and you can just annoy the others!')
+            rospy.loginfo('You are a joker and you can just annoy the others!')
 
     def goalReceivedCallback(self, goal_msg):
-        print('Received new goal')
-        self.goal = goal_msg   # storing goal inside the class
+        rospy.loginfo('Received new goal')
+        self.goal = goal_msg  # storing goal inside the class
         self.goal_active = True
 
     def sendCommandCallback(self, event):
-        print('Sending twist command')
         # Decision outputs a speed (linear Velocity) and an angle (angular Velocity)
         # input: goal
         # output: angle and speed
@@ -78,8 +89,8 @@ class Driver():
                 angle = 0
                 speed = 0
         else:
-            angle = 0
-            speed = 0
+            angle = 1
+            speed = 1
 
         # Build the command message (Twist) and publish it
         twist = Twist()
@@ -92,7 +103,7 @@ class Driver():
         goal_present_time = copy.deepcopy(goal)
         goal_present_time.header.stamp = rospy.Time.now()
 
-        target_frame = 'p_randomName/base_footprint'
+        target_frame = self.name + '/base_footrospy.loginfo'
         try:
             goal_in_base_link = self.tf_buffer.transform(goal_present_time, target_frame, rospy.Duration(1))
         except(tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
@@ -102,16 +113,16 @@ class Driver():
         x = goal_in_base_link.pose.position.x
         y = goal_in_base_link.pose.position.y
 
-        distance = math.sqrt(x**2 + y**2)
+        distance = math.sqrt(x ** 2 + y ** 2)
 
         return distance
 
-    def driveStraight(self, goal, min_speed = 0.1, max_speed = 0.5):
+    def driveStraight(self, goal, min_speed=0.1, max_speed=0.5):
 
         goal_present_time = copy.deepcopy(goal)
         goal_present_time.header.stamp = rospy.Time.now()
 
-        target_frame = 'p_randomName/base_footprint'
+        target_frame = self.name + '/base_footrospy.loginfo'
         try:
             goal_in_base_link = self.tf_buffer.transform(goal_present_time, target_frame, rospy.Duration(1))
         except(tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
@@ -121,10 +132,10 @@ class Driver():
         x = goal_in_base_link.pose.position.x
         y = goal_in_base_link.pose.position.y
 
-        angle = math.atan2(y, x) # compute the angle
+        angle = math.atan2(y, x)  # compute the angle
 
         # Define the linear speed based on the distance to the goal
-        distance = math.sqrt(x**2 + y**2)
+        distance = math.sqrt(x ** 2 + y ** 2)
         speed = 0.5 * distance
 
         # saturate speed to minimum and maximum values
@@ -132,6 +143,10 @@ class Driver():
         speed = max(speed, min_speed)
 
         return angle, speed
+
+
+
+
 def main():
     # ------------------------------------------------------
     # Initialization
