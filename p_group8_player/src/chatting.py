06c3a036512664c01a59e1c73e900bf67678fb00
GabrieleMicheli <gabriele.micheli@ua.pt>
2022-02-23 16:13:11 +0000

    #11 "Insults" other players
    Created the *PSR p_group8 TeamHunt* chat using *tkinder*.
    Now every time we start the game the chat is shown.
    I start the node *chatting* inside the *game_bringup.launch* file as we did for the *driver* node inside the *bringup.launch* file.
    
    Next steps:
    -create a bar to scroll through the text when the chat is full
    -define the strings of information to print (ex. 'R1: Oh no, run!' and 'B1: I'm coming brother!')

--- p_group8_player/src/chatting.py
+++ p_group8_player/src/chatting.py
@@ -1,38 +1,41 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
+# IMPORTS
+import rospy
 from tkinter import *  # Import tkinter library
 
-
-def moveWindowCenter(window):
-    winWidth = window.winfo_reqwidth()
-    winwHeight = window.winfo_reqheight()
-    posRight = int(window.winfo_screenwidth() / 2 - winWidth / 2)
-    posDown = int(window.winfo_screenheight() / 2 - winwHeight / 2)
-    window.geometry("+{}+{}".format(posRight, posDown))
-
-
 def main():
-    # declare the window
-    window = Tk()
-    # set window title
-    window.title('TeamHunt chat')
-    # set window width and height
-    window.configure(width=500, height=300)
-    # set window background color
-    window.configure(bg='white')
+    rospy.init_node('chatting_node')
+    rate = rospy.Rate(1)
 
-    # if red is hunted from blue -> ex. text = 'R1: Oh no, run!' and text = 'B1: I am coming bro!'
-    lbl = Label(window, text='R1: Oh no, run!', bg='White', fg='Red')  # not use label but something different
-    # if blue catches red -> ex. text = 'B1: Game over bro!' and text = 'R1: :('
+    while not rospy.is_shutdown():
+        try:
+            window = Tk()  # declare the window
+            window.title('PSR TeamHunt p_group8 chat')  # set window title
+            window.configure(width=500, height=300)  # set window width and height
+            window.configure(bg='white')  # set window background color
 
-    lbl.place(x=20, y=10)
+            # if red is hunted from blue -> ex. text = 'R1: Oh no, run!' and text = 'B1: I am coming bro!'
+            lbl = Label(window, text='R1: Oh no, run!', bg='White', fg='Red')  # not use label but something different
+            # if blue catches red -> ex. text = 'B1: Game over bro!' and text = 'R1: :('
 
-    moveWindowCenter(window)
+            lbl.place(x=20, y=10)
 
-    window.mainloop()
+            winWidth = window.winfo_reqwidth()
+            winwHeight = window.winfo_reqheight()
+            posRight = int(window.winfo_screenwidth() / 2 - winWidth / 2)
+            posDown = int(window.winfo_screenheight() / 2 - winwHeight / 2)
+            window.geometry("+{}+{}".format(posRight, posDown))
+
+            window.mainloop()
+
+            rate.sleep()
+        except rospy.ServiceException as e:
+            print('Service call failed %s', e)
 
     # TODO: change line and create bar to scroll the text when chat is full
     # TODO: after defined somewhere in the program the boolean variables to se if the robot is chatching o is chatched implement the informations strings
+    # TODO: if I close the chat, the chat as to remain closed
 
 
 if __name__ == '__main__':
