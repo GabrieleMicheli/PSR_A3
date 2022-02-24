fd3e1ea90788473c014513cbb2824b1e019c5745
GabrieleMicheli <gabriele.micheli@ua.pt>
2022-02-24 15:33:26 +0000

    #11 "Insults" other players
    Fixed the stupid problem: "If I close the chat, the chat as to remain closed"
    Created a bar to scroll through the text when the chat is full
    
    Next steps:
    -define the strings of information to print (ex. 'R1: Oh no, run!' and 'B1: I'm coming brother!')
    -create a button to clear the chat

--- p_group8_player/src/chatting.py
+++ p_group8_player/src/chatting.py
@@ -6,61 +6,49 @@ from tkinter import *  # Import tkinter library
 from tkinter import ttk
 
 def main():
-    rospy.init_node('chatting_node')
-    rate = rospy.Rate(1)
+    rospy.init_node('chatting_node') # init chatting node
 
-    while not rospy.is_shutdown():
-        try:
-            window = Tk()  # declare the window
-            window.title('PSR TeamHunt p_group8 chat')  # set window title
-            window.configure(width=500, height=300,bg='white')  # set window width and height
+    window = Tk()  # declare the window
+    window.title('PSR TeamHunt p_group8 chat')  # set window title
+    window.configure(width=500, height=300,bg='white')  # set window width and height
 
-            # create a main frame
-            main_frame = Frame(window)
-            main_frame.pack(fill=BOTH, expand=1)
-            main_frame.configure(bg='white')  # set window background color
+    # create a main frame
+    main_frame = Frame(window)
+    main_frame.pack(fill=BOTH, expand=1)
+    main_frame.configure(bg='white')  # set window background color
 
-            # create a canvas
-            my_canvas = Canvas(main_frame)
-            my_canvas.pack(side=LEFT, fill=BOTH, expand=1)
-            my_canvas.configure(bg='white')
-    
-            # add a scrollbar to the canvas
-            my_scrollbar = ttk.Scrollbar(main_frame, orient=VERTICAL, command=my_canvas.yview)
-            my_scrollbar.pack(side=RIGHT, fill=Y)
-    
-            # configure the canvas
-            my_canvas.configure(yscrollcommand=my_scrollbar.set)
-            my_canvas.bind('<Configure>', lambda e: my_canvas.configure(scrollregion=my_canvas.bbox("all")))
-    
-            # create another frame inside the canvas
-            second_frame = Frame(my_canvas)
-            second_frame.configure(bg='white')  # set window background color
+    # create a canvas
+    my_canvas = Canvas(main_frame)
+    my_canvas.pack(side=LEFT, fill=BOTH, expand=1)
+    my_canvas.configure(bg='white')
 
-            # add that new frame to a window in the canvas
-            my_canvas.create_window((0, 0), window=second_frame, anchor="nw")
+    # add a scrollbar to the canvas
+    my_scrollbar = ttk.Scrollbar(main_frame, orient=VERTICAL, command=my_canvas.yview)
+    my_scrollbar.pack(side=RIGHT, fill=Y)
 
-            label = Label(second_frame, text='R1: Oh no, run!\n', bg='White', fg='Red').grid(row=1, column=1)
+    # configure the canvas
+    my_canvas.configure(yscrollcommand=my_scrollbar.set)
+    my_canvas.bind('<Configure>', lambda e: my_canvas.configure(scrollregion=my_canvas.bbox("all")))
 
-            # handle WM_DELETE_WINDOW event
-            def handleProtocol():
-                # open a dialog
-                # if tkinter.messagebox.askokcancel("Notice", "Are you sure to close the window"):
-                # close the application
-                window.destroy()
+    # create another frame inside the canvas
+    second_frame = Frame(my_canvas)
+    second_frame.configure(bg='white')  # set window background color
 
-            window.protocol("WM_DELETE_WINDOW", handleProtocol)
+    # add that new frame to a window in the canvas
+    my_canvas.create_window((0, 0), window=second_frame, anchor="nw")
 
-            window.mainloop()
-            rate.sleep()
+    label = Label(second_frame, text='R1: Oh no, run!\n', bg='White', fg='Red').grid(row=1, column=1)
 
-        except rospy.ServiceException as e:
-            print('Service call failed %s', e)
+    def handleProtocol(): # handle WM_DELETE_WINDOW event
+        window.destroy()
 
-    # TODO: change line
-    # TODO: after defined somewhere in the program the boolean variables to se if the robot is chatching o is chatched implement the informations strings
-    # TODO: if I close the chat, the chat as to remain closed
-    # TODO: create a button to clear the chat
+    window.protocol("WM_DELETE_WINDOW", handleProtocol)
+
+    window.mainloop()
+
+# TODO: change line
+# TODO: after defined somewhere in the program the boolean variables to se if the robot is chatching o is chatched implement the informations strings
+# TODO: create a button to clear the chat
 
 if __name__ == '__main__':
     main()
