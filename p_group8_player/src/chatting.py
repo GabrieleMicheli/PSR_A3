c71451e15edc450e33bd1b8c066befbab81b69ca
GabrieleMicheli <gabriele.micheli@ua.pt>
2022-02-25 17:41:44 +0100

    #11 "Insults" other players
    Published /R1/state from driver node, subscribed /R1/state and print the state
    
    Next steps:
    -define the strings of information to print (ex. 'R1: Oh no, run!' and 'B1: I'm coming brother!')

--- p_group8_player/src/chatting.py
+++ p_group8_player/src/chatting.py
@@ -4,6 +4,7 @@
 import rospy
 from tkinter import *  # Import tkinter library
 from tkinter import ttk
+from std_msgs.msg import String
 
 def create_window(title, width, height, backGroundColour):
     window = Tk()  # declare the window
@@ -32,8 +33,13 @@ def addScrollbarToCanvas(frame, canvas):
 def clear_label(label):
     label.destroy()
 
+def robot_state_callback(robot_state):
+    rospy.loginfo(robot_state.data) # print /R1/state
+
 def main():
     rospy.init_node('chatting_node') # init chatting node
+
+    robot_state_subscriber = rospy.Subscriber('/R1/state', String, robot_state_callback)
 
     # defining window properties
     window_title = 'PSR TeamHunt p_group8 chat'; width = 500; height = 800; backGroundColour = 'white'
@@ -57,7 +63,18 @@ def main():
     # add that new frame to a window in the canvas
     canvas.create_window((0, 0), window=second_frame, anchor="nw")
 
+    # if robot_state_msg.__eq__('wait'):
+    #     label_text = 'What a deadly bore'
+    # elif robot_state_msg.__eq__('attack'):
+    #     label_text = 'I am very hungry'
+    # elif robot_state_msg.__eq__('flee'):
+    #     label_text = 'Oh no, I have to run'
+    # else:
+    #     label_text = 'Walls everywhere in this game'
+
     label = Label(second_frame, text='R1: Oh no, run!\n', bg='White', fg='Red') # creating a label
+    # label = Label(second_frame, text=label_text, bg='White', fg='Red') # creating a label
+
     label.grid(row=1, column=1) # label positioning
 
     # create a button to clear the chat
