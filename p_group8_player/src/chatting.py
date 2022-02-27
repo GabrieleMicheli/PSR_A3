39ccd69e6c453536a1e8560130815b8dfe0a64dd
GabrieleMicheli <gabriele.micheli@ua.pt>
2022-02-27 16:37:58 +0100

    #11 "Insults" other players
    Next steps:
    -define the strings of information to print (ex. 'R1: Oh no, run!' and 'B1: I'm coming brother!') in the chat box

--- p_group8_player/src/chatting.py
+++ p_group8_player/src/chatting.py
@@ -14,18 +14,22 @@ class Chatting():
         self.second_frame = None
         self.canvas = None
         self.label = None
-        self.label_text = 'No label text'
+        self.label_text = None
+        self.player_label = None
+        self.robot_state = None
+        self.robot_state_list = []
+
         self.clear_button = None
         self.stop_flag = False
+        self.robot_state_changed = False # boolean variable to check if the robot state is changed
 
         # SUBSCRIBER
         self.robot_state_subscriber = None
-        self.actual_robot_state = None
 
-    def windowConfiguration(self, title, width, height, backGroundColour):
+    def windowConfiguration(self, title, width, height, bg_colour):
         self.window = Tk() # declare the window
         self.window.title(title)  # set window title
-        self.window.configure(width=width, height=height,bg=backGroundColour)  # set window width, height and back ground colour
+        self.window.configure(width=width, height=height,bg=bg_colour)  # set window width, height and back ground colour
 
     def mainFrameConfiguration(self, bg_colour):
         self.main_frame = Frame(self.window)
@@ -50,10 +54,11 @@ class Chatting():
 
     def robotStateCallback(self, robot_state_msg):
         rospy.loginfo(robot_state_msg.data) # print /R1/state
-        actual_robot_state = robot_state_msg.data
+        # return robot_state_msg.data
 
-    def robotStatesCallback(self, robot_states_msg):
-        rospy.loginfo(robot_states_msg.data) # print /robot_name/state
+    # def robotStateCallback(self, robot_state_msg):
+    #     # rospy.loginfo(rospy.get_caller_id() + 'I heard' + robot_state_msg.data)  # NOT WORKING: probably because inside a for loop?
+    #     return robot_state_msg.data
 
     def getRobotStates(self):
         # Getting robot names
@@ -61,8 +66,19 @@ class Chatting():
         green_players_list = rospy.get_param('/green_players')
         blue_players_list = rospy.get_param('/blue_players')
         players_list = [*red_players_list, *green_players_list, *blue_players_list]
+
         for i in range(players_list):
-            rospy.Subscriber('/' + players_list[i] + '/' + 'state', String, self.robotStatesCallback)
+            self.robot_state = rospy.Subscriber('/' + players_list[i] + '/' + 'state', String, self.robotStateCallback)
+        #     print
+        #
+        # print(self.robot_state_list)
+
+    def robotStateChangeControl(self, actual_robot_state, old_robot_state):
+        if actual_robot_state != old_robot_state:
+            # rospy.loginfo('Robot status changed')
+            self.robot_state_changed = True
+        else:
+            self.robot_state_changed = False
 
     def getLabel(self, robot_state_msg):
         if robot_state_msg.__eq__('wait'):
@@ -91,7 +107,17 @@ class Chatting():
 
         while not self.stop_flag:
 
-            self.robot_state_subscriber = rospy.Subscriber('/R1/state', String, self.robotStateCallback)
+            # self.robot_state_subscriber = rospy.Subscriber('/R1/state', String, self.robotStateCallback)
+
+            # self.robotStateChangeControl(old_robot_state)
+
+            # if self.robotStateChangeControl(old_robot_state):
+            #     print(self.robot_state_subscriber)
+            #     print('Status changed')
+            # else:
+            #     print('Status not changed')
+            #
+            # old_robot_state = self.robot_state_subscriber
 
             self.getLabel(self.robot_state_subscriber)
             self.label = Label(self.second_frame, text=self.label_text, bg='White', fg='Red')  # creating a label
@@ -115,8 +141,6 @@ class Chatting():
 def main():
 
     rospy.init_node('chatting_node') # init chatting node
-
-
 
     # DEFINING WINDOW PROPERTIES
     window_title = 'PSR TeamHunt p_group8 chat'; width = 500; height = 800
