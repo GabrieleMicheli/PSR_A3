7d8948fb6aa3da6b3bbea4547bca400bd1f06441
GabrieleMicheli <93253340+GabrieleMicheli@users.noreply.github.com>
2022-03-02 23:45:57 +0100

    Update chatting.py

--- p_group8_player/src/chatting.py
+++ p_group8_player/src/chatting.py
@@ -2,11 +2,13 @@
 
 import rospy
 from std_msgs.msg import String
+from colorama import Fore, Style
+
 
 class Chatting:
     def __init__(self):
-        # Get parameters from YAML file
         self.state_msg = None
+        # Get parameters from YAML file
         red_players_list = rospy.get_param('/red_players')
         green_players_list = rospy.get_param('/green_players')
         blue_players_list = rospy.get_param('/blue_players')
@@ -15,29 +17,31 @@ class Chatting:
         players_list = [*red_players_list, *green_players_list, *blue_players_list]
 
         for player in players_list:
-            robot_state_subscriber = rospy.Subscriber('/' + player + '/state_msg', String, self.robot_state_callback)
+            rospy.Subscriber('/' + player + '/state_msg', String, self.robot_state_callback)
 
     def robot_state_callback(self, state_msg):
-        # if
-        print(state_msg.data)
+        if 'R' in state_msg.data:
+            print(Fore.RED + state_msg.data + Style.RESET_ALL)
+        elif 'G' in state_msg.data:
+            print(Fore.GREEN + state_msg.data + Style.RESET_ALL)
+        else:
+            print(Fore.BLUE + state_msg.data + Style.RESET_ALL)
         self.state_msg = state_msg.data
 
-    def chat_creation(self): # NOW is printing messages only when the state changes
+    def chat_creation(self):
         if not self.state_msg.__eq__(None):
             print(self.state_msg)
-
-
-# TODO: By actions, print a specific message!
 
 
 def main():
     rospy.init_node('chatting_node')  # init chatting node
     chatting = Chatting()
-    rate = rospy.Rate(1)
 
     while not rospy.is_shutdown():
         chatting.chat_creation()
     rospy.spin()
 
+
 if __name__ == '__main__':
     main()
+
