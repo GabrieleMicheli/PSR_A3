2811ab9dffcf330e11332914b93871a635cbb8bf
josepedropinto-pt <josemcp@ua.pt>
2022-03-02 18:08:44 +0000

    merge sensors almost done

--- p_group8_player/src/driver_functions.py
+++ p_group8_player/src/driver_functions.py
@@ -2,55 +2,28 @@
 
 import cv2
 
-def findCentroid(mask_original):
-    """
-    Create a mask with the largest blob of mask_original and return its centroid coordinates
-    :param mask_original: Cv2 image - Uint8
-    :return mask: Cv2 image - Bool
-    :return centroid: List of 2 values
-    """
 
-    # Defining maximum area and mask label
-    maxArea = 0
-    maxLabel = 0
+def callbackPodium(image):
+    try:
+        img = cv2.imread(image, 1)
+        cv2.namedWindow('Winning Team', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Winning Team', 1200, 675)
 
-    # You need to choose 4 or 8 for connectivity type
-    connectivity = 4
+        first_place = cv2.imread('green.jpg', 1)
+        first_place = cv2.resize(first_place, (160, 90), interpolation=cv2.INTER_AREA)
 
-    # Perform the operation
-    output = cv2.connectedComponentsWithStats(mask_original, connectivity, cv2.CV_32S)
+        second_place = cv2.imread('blue.jpg', 1)
+        second_place = cv2.resize(second_place, (100, 70), interpolation=cv2.INTER_AREA)
 
-    # Get the results
-    # The first cell is the number of labels
-    num_labels = output[0]
+        third_place = cv2.imread('red.jpg', 1)
+        third_place = cv2.resize(third_place, (100, 70), interpolation=cv2.INTER_AREA)
 
-    # The second cell is the label matrix
-    labels = output[1]
+        img[100:190, 180:340] = first_place
+        img[130:200, 50:150] = second_place
+        img[140:210, 375:475] = third_place
+        cv2.putText(img, 'Team Hunting Olympics - Aveiro 2022', (80, 310),
+                    fontFace=cv2.FONT_ITALIC, fontScale=0.5, color=(100, 50, 200), thickness=2)
+        return img
 
-    # The third cell is the stat matrix
-    stats = output[2]
-
-    # The fourth cell is the centroid matrix
-    centroids = output[3]
-
-    # For each blob, find their area and compare it to the largest one
-    for label in range(1, num_labels):
-        # Find area
-        area = stats[label, cv2.CC_STAT_AREA]
-
-        # If the area is larger then the max area to date, replace it
-        if area > maxArea:
-            maxArea = area
-            maxLabel = label
-
-    # If there are blobs, the label is different than zero
-    if maxLabel != 0:
-        # Create a new mask and find its centroid
-        mask = labels == maxLabel
-        centroid = centroids[maxLabel]
-    else:
-        # If there are no blobs, the mask stays the same, and there are no centroids
-        mask = mask_original
-        centroid = None
-
-    return mask, centroid
+    except Exception as e:
+        print(str(e))
